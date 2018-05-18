767eb2fdf2611e3feadf1cbbec274267cc00ad67
karissa <krmckelv@gmail.com>
2018-05-17 20:01:20 -0500

    add back community dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const xhr = require('xhr')
 const css = require('sheetify')
+const querystring = require('querystring')
 const mapboxgl = require('mapbox-gl')
 const lozad = require('lozad')()
+const elements = require('alianza-elements')
 const DigidemAttrib = require('@digidem/attribution-control')
 
+const communityDOM = require('./community_popup')
 const sidebarDOM = require('./sidebar')
 const urls = require('../static/urls.json')
 
@@ -19,9 +22,10 @@ if (process.env.NODE_ENV === 'production') {
     })
   }
 }
-
 var data = {}
 var dataIndex = {}
+var qs = querystring.parse(window.location.search.replace('?', ''))
+var lang = qs.lang || 'es'
 
 css('mapbox-gl/dist/mapbox-gl.css')
 css('alianza-elements/style.css')
@@ -42,7 +46,7 @@ var map = window.map = new mapboxgl.Map({
   scrollZoom: false,
   boxZoom: false,
   doubleClickZoom: false
-})
+}).on('load', onLoad)
 
 xhr('data.json', {header: {
   'Content-Type': 'application/json'
@@ -54,6 +58,7 @@ xhr('data.json', {header: {
       dataIndex[feature.properties.Preset] = feature
     })
   })
+  onLoad()
 })
 
 map.addControl(new mapboxgl.ScaleControl(), 'top-right')
@@ -65,3 +70,34 @@ map.on('style.load', function () {
   document.body.appendChild(sidebar)
   lozad.observe()
 })
+
+var communityPopup = elements.popup(map)
+
+function onLoad () {
+  // When a click event occurs near a place, open a popup at the location of
+  // the feature, with description HTML from its properties.
+  map.on('click', function (e) {
+    var _features = map.queryRenderedFeatures(e.point, {filter: ['!=', '$id', 1]})
+    var feature = _features && _features[0]
+    if (feature) {
+      var coords = feature.geometry.type === 'Point' ? feature.geometry.coordinates : map.unproject(e.point)
+      var opts = {feature, lang}
+      var preset = feature.properties.preset
+      if (preset) {
+        var airtable = dataIndex[preset.toLowerCase()]
+        opts.data = airtable ? airtable.properties : {}
+      }
+      communityPopup.update(communityDOM(opts))
+      communityPopup.setLngLat(coords)
+      return
+    }
+    communityPopup.remove()
+  })
+
+  // Use the same approach as above to indicate that the symbols are clickable
+  // by changing the cursor style to 'pointer'.
+  map.on('mousemove', function (e) {
+    var features = map.queryRenderedFeatures(e.point)
+    map.getCanvas().style.cursor = (features.length) ? 'pointer' : ''
+  })
+}
