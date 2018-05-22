ee6fa0e246ce22e865a3dbd38b06320d29e9b0df
Gregor MacLennan <gmaclennan@digital-democracy.org>
2018-05-22 01:02:03 +0100

    Fade layers in and out between flytos

--- src/map_transition.js
+++ src/map_transition.js
@@ -1,9 +1,11 @@
 var views = require('./map_views.json')
 
+var timeoutId
+var FADE_DURATION = 1000
+
 var overlays = [
   'for-conflict-layer-block',
   'for-conflict-layer-petrol',
-  'background',
   'territory-outline',
   'zona-peces',
   'zona-animales',
@@ -42,35 +44,62 @@ module.exports = mapTransition
 function mapTransition (viewId, map) {
   var view = views[viewId]
   if (!view) return console.log('undefined view', viewId)
-  if (!map.loaded()) return
+  if (!map.loaded()) return console.log('not loaded', viewId)
 
   map.stop()
   map.off('moveend', showOverlays)
+  if (timeoutId) clearTimeout(timeoutId)
 
-  hideOverlays()
+  fadeOverlays(0)
+  timeoutId = setTimeout(function () {
+    hideOverlays()
+    moveMap()
+  }, FADE_DURATION)
 
-  map.flyTo({
-    center: view.center,
-    zoom: view.zoom,
-    speed: 0.3
-  })
   map.on('moveend', showOverlays)
 
   function showOverlays () {
     Object.keys(view.layerVisibility).forEach(function (layerId) {
+      if (!map.getLayer(layerId)) return console.log('no layer', layerId)
       var currentVisibility = map.getLayoutProperty(layerId, 'visibility')
       var targetVisibility = view.layerVisibility[layerId]
-      if (targetVisibility === currentVisibility) return
+      if (currentVisibility === 'none' && targetVisibility === 'visible') {
+        console.log('opacity 0', layerId)
+        setLayerOpacity(map, layerId, 0, 0)
+      }
       map.setLayoutProperty(layerId, 'visibility', targetVisibility)
+    })
+    fadeOverlays(1)
+  }
+
+  function fadeOverlays (opacity) {
+    overlays.forEach(function (layerId) {
+      if (!map.getLayer(layerId)) return console.log('no layer', layerId)
+      // if (map.getLayoutProperty(layerId, 'visibility') === 'none') return
+      setLayerOpacity(map, layerId, opacity)
+    })
+  }
+
+  function moveMap () {
+    map.flyTo({
+      center: view.center,
+      zoom: view.zoom,
+      speed: 0.3
     })
   }
 
   function hideOverlays () {
     overlays.forEach(function (layerId) {
       if (!map.getLayer(layerId)) return console.log('no layer', layerId)
-      var visibility = map.getLayoutProperty(layerId, 'visibility')
-      if (visibility === 'none') return
       map.setLayoutProperty(layerId, 'visibility', 'none')
     })
   }
+}
+
+function setLayerOpacity (map, layerId, opacity, duration) {
+  if (typeof duration === 'undefined') duration = FADE_DURATION
+  var layer = map.getLayer(layerId)
+  var propName = layer.type === 'symbol' ? 'icon-opacity' : layer.type + '-opacity'
+  map.setPaintProperty(layerId, propName + '-transition', {duration: duration, delay: 0})
+  map.setPaintProperty(layerId, propName, opacity)
 }
