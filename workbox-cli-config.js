d74bdf3e2c6e611288ccdda6bad79f9c43d02f00
Gregor MacLennan <gmaclennan@digital-democracy.org>
2018-05-23 16:42:43 +0100

    Remove sw caching of maptiles
    mapbox will request from different subdomains every time, this means that we fill peoples' caches with lots of different copies of the same thing. We'll fix this later, turning off for launch

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -6,15 +6,15 @@ module.exports = {
   'swDest': 'dist/sw.js',
   'skipWaiting': true,
   'runtimeCaching': [{
-    // You can use a RegExp as the pattern:
-    urlPattern: /https:\/\/api\.mapbox\.com|https:\/\/[abcd]\.tiles\.mapbox\.com|https:\/\/ecn\.t\d\.tiles\.virtualearth\.net/,
-    handler: 'staleWhileRevalidate',
-    // Any options provided will be used when
-    // creating the caching strategy.
-    options: {
-      cacheName: 'map-cache',
-      cacheableResponse: {statuses: [0, 200]}
-    }
+    // // You can use a RegExp as the pattern:
+    // urlPattern: /https:\/\/api\.mapbox\.com|https:\/\/[abcd]\.tiles\.mapbox\.com|https:\/\/ecn\.t\d\.tiles\.virtualearth\.net/,
+    // handler: 'staleWhileRevalidate',
+    // // Any options provided will be used when
+    // // creating the caching strategy.
+    // options: {
+    //   cacheName: 'map-cache',
+    //   cacheableResponse: {statuses: [0, 200]}
+    // }
   }],
   'globIgnores': [
     '../workbox-cli-config.js'
