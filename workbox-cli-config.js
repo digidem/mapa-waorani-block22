61c62d59a48d6f7937547e3f8966a09f3c8bf917
karissa <krmckelv@gmail.com>
2018-05-23 11:07:05 -0500

    remove runtime caching completely

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -5,7 +5,7 @@ module.exports = {
   ],
   'swDest': 'dist/sw.js',
   'skipWaiting': true,
-  'runtimeCaching': [{
+//  'runtimeCaching': [{
     // // You can use a RegExp as the pattern:
     // urlPattern: /https:\/\/api\.mapbox\.com|https:\/\/[abcd]\.tiles\.mapbox\.com|https:\/\/ecn\.t\d\.tiles\.virtualearth\.net/,
     // handler: 'staleWhileRevalidate',
@@ -15,7 +15,7 @@ module.exports = {
     //   cacheName: 'map-cache',
     //   cacheableResponse: {statuses: [0, 200]}
     // }
-  }],
+//  }],
   'globIgnores': [
     '../workbox-cli-config.js'
   ]
