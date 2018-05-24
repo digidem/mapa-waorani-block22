b6101117be1103a1bf35526fadceabf94966022a
Gregor MacLennan <gmaclennan@digital-democracy.org>
2018-05-24 17:36:54 +0100

    Use default workbox cache names

--- static/sw.js
+++ static/sw.js
@@ -7,23 +7,17 @@ workbox.clientsClaim()
 
 workbox.routing.registerRoute(
   /https:\/\/[abcd]\.tiles\.mapbox\.com/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'mapbox-tile-cache'
-  })
+  new workbox.strategies.CacheFirst()
 )
 
 workbox.routing.registerRoute(
   /https:\/\/ecn\.t\d\.tiles\.virtualearth\.net/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'bing-tile-cache'
-  })
+  new workbox.strategies.CacheFirst()
 )
 
 workbox.routing.registerRoute(
   /https:\/\/resizer.digital-democracy.org/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'images'
-  })
+  new workbox.strategies.CacheFirst()
 )
 
 workbox.precaching.precacheAndRoute([])
